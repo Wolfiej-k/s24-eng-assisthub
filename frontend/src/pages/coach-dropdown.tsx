ee75625fb5305416276684348924ab2a87d908b8
michelleln <michelleln@users.noreply.github.com>
2024-03-26 00:23:53 +0000

    Apply formatting changes

--- frontend/src/pages/coach-dropdown.tsx
+++ frontend/src/pages/coach-dropdown.tsx
@@ -1,13 +1,13 @@
 import Autocomplete from "@mui/material/Autocomplete"
+import Button from "@mui/material/Button"
 import TextField from "@mui/material/TextField"
+import type { GridColDef, GridValueGetterParams } from "@mui/x-data-grid"
+import { DataGrid } from "@mui/x-data-grid"
 import { useUpdate } from "@refinedev/core"
+import { useDataGrid } from "@refinedev/mui"
+import { useState } from "react"
 import { type CaseItem, type Coach } from "../types"
-import { useDataGrid } from "@refinedev/mui";
-import { DataGrid } from "@mui/x-data-grid";
-import type { GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
-import React, { useState } from "react";
-import DetailedCaseView from "./detailed-case-view";
-import Button from '@mui/material/Button';
+import DetailedCaseView from "./detailed-case-view"
 
 export default function CoachDropdown({ item }: { item: CaseItem }) {
   const { mutate } = useUpdate()
@@ -39,124 +39,84 @@ export default function CoachDropdown({ item }: { item: CaseItem }) {
       field: "clientName",
       headerName: "Client Name",
       width: 90,
-      valueGetter: (params: GridValueGetterParams) =>
-      (params.row as CaseItem).client?.name ?? '',
-      renderCell: (params) => (
-        <div style={{ whiteSpace: 'normal', wordWrap: 'break-word' }}>
-          {params.value}
-        </div>
-      )
+      valueGetter: (params: GridValueGetterParams) => (params.row as CaseItem).client?.name ?? "",
+      renderCell: (params) => <div style={{ whiteSpace: "normal", wordWrap: "break-word" }}>{params.value}</div>,
     },
     {
       field: "clientEmail",
       headerName: "Client Email",
       width: 200,
-      valueGetter: (params: GridValueGetterParams) =>
-      (params.row as CaseItem).client?.email ?? '',
-      renderCell: (params) => (
-        <div style={{ whiteSpace: 'normal', wordWrap: 'break-word' }}>
-          {params.value}
-        </div>
-      )
+      valueGetter: (params: GridValueGetterParams) => (params.row as CaseItem).client?.email ?? "",
+      renderCell: (params) => <div style={{ whiteSpace: "normal", wordWrap: "break-word" }}>{params.value}</div>,
     },
     {
       field: "clientPhone",
       headerName: "Client Phone",
       width: 130,
-      valueGetter: (params: GridValueGetterParams) =>
-      (params.row as CaseItem).client?.phone ?? '',
-      renderCell: (params) => (
-        <div style={{ whiteSpace: 'normal', wordWrap: 'break-word' }}>
-          {params.value}
-        </div>
-      )
+      valueGetter: (params: GridValueGetterParams) => (params.row as CaseItem).client?.phone ?? "",
+      renderCell: (params) => <div style={{ whiteSpace: "normal", wordWrap: "break-word" }}>{params.value}</div>,
     },
     {
       field: "clientZip",
       headerName: "ZIP Code",
       width: 70,
-      valueGetter: (params: GridValueGetterParams) =>
-      (params.row as CaseItem).client?.zip ?? '',
-      renderCell: (params) => (
-        <div style={{ whiteSpace: 'normal', wordWrap: 'break-word' }}>
-          {params.value}
-        </div>
-      )
+      valueGetter: (params: GridValueGetterParams) => (params.row as CaseItem).client?.zip ?? "",
+      renderCell: (params) => <div style={{ whiteSpace: "normal", wordWrap: "break-word" }}>{params.value}</div>,
     },
     {
       field: "clientProfile",
       headerName: "Profile URL",
       width: 200,
-      valueGetter: (params: GridValueGetterParams) =>
-      (params.row as CaseItem).client?.profile ?? '' as string,
+      valueGetter: (params: GridValueGetterParams) => (params.row as CaseItem).client?.profile ?? ("" as string),
       renderCell: (params) => {
-        const profileUrl = (params.value as string).startsWith('https://')? params.value as string: `https://${params.value}`;
+        const profileUrl = (params.value as string).startsWith("https://")
+          ? (params.value as string)
+          : `https://${params.value}`
         return (
-          <div style={{ whiteSpace: 'normal', wordWrap: 'break-word' }}>
+          <div style={{ whiteSpace: "normal", wordWrap: "break-word" }}>
             {/* Use the adjusted profileUrl variable */}
-            <a href={profileUrl} target="_blank" rel="noopener noreferrer">{params.value}</a>
+            <a href={profileUrl} target="_blank" rel="noopener noreferrer">
+              {params.value}
+            </a>
           </div>
-          );
-        }
+        )
+      },
     },
     {
       field: "coachesNames",
       headerName: "Coaches",
       width: 200,
       valueGetter: (params: GridValueGetterParams) =>
-      (params.row as CaseItem).coaches?.map(coach => coach.name).join(', ') || 'None',
-      renderCell: (params) => (
-        <div style={{ whiteSpace: 'normal', wordWrap: 'break-word' }}>
-          {params.value}
-        </div>
-      )
+        (params.row as CaseItem).coaches?.map((coach) => coach.name).join(", ") || "None",
+      renderCell: (params) => <div style={{ whiteSpace: "normal", wordWrap: "break-word" }}>{params.value}</div>,
     },
     {
       field: "startTime",
       headerName: "Start Time",
       width: 150,
       valueGetter: (params: GridValueGetterParams) => new Date((params.row as CaseItem).startTime).toLocaleString(),
-      renderCell: (params) => (
-        <div style={{ whiteSpace: 'normal', wordWrap: 'break-word' }}>
-          {params.value}
-        </div>
-      )
+      renderCell: (params) => <div style={{ whiteSpace: "normal", wordWrap: "break-word" }}>{params.value}</div>,
     },
     {
       field: "language",
       headerName: "Language",
       width: 120,
-      valueGetter: (params: GridValueGetterParams) =>
-      (params.row as CaseItem).data?.language ?? '',
-      renderCell: (params) => (
-        <div style={{ whiteSpace: 'normal', wordWrap: 'break-word' }}>
-          {params.value}
-        </div>
-      )
+      valueGetter: (params: GridValueGetterParams) => (params.row as CaseItem).data?.language ?? "",
+      renderCell: (params) => <div style={{ whiteSpace: "normal", wordWrap: "break-word" }}>{params.value}</div>,
     },
     {
       field: "benefits",
       headerName: "Benefits",
       width: 180,
-      valueGetter: (params: GridValueGetterParams) =>
-      (params.row as CaseItem).data?.benefits ?? '',
-      renderCell: (params) => (
-        <div style={{ whiteSpace: 'normal', wordWrap: 'break-word' }}>
-          {params.value}
-        </div>
-      )
+      valueGetter: (params: GridValueGetterParams) => (params.row as CaseItem).data?.benefits ?? "",
+      renderCell: (params) => <div style={{ whiteSpace: "normal", wordWrap: "break-word" }}>{params.value}</div>,
     },
     {
       field: "notes",
       headerName: "Notes",
       width: 180,
-      valueGetter: (params: GridValueGetterParams) =>
-      (params.row as CaseItem).notes ?? '',
-      renderCell: (params) => (
-        <div style={{ whiteSpace: 'normal', wordWrap: 'break-word' }}>
-          {params.value}
-        </div>
-      )
+      valueGetter: (params: GridValueGetterParams) => (params.row as CaseItem).notes ?? "",
+      renderCell: (params) => <div style={{ whiteSpace: "normal", wordWrap: "break-word" }}>{params.value}</div>,
     },
     {
       field: "actions",
@@ -164,32 +124,37 @@ export default function CoachDropdown({ item }: { item: CaseItem }) {
       sortable: false,
       width: 150,
       renderCell: (params) => (
-        <Button size="small" variant="contained" color="primary" onClick={() => setSelectedCase(params.row as CaseItem)}
-        sx={{
-          fontSize: '0.6rem',
-        }}>
+        <Button
+          size="small"
+          variant="contained"
+          color="primary"
+          onClick={() => setSelectedCase(params.row as CaseItem)}
+          sx={{
+            fontSize: "0.6rem",
+          }}
+        >
           View Details
         </Button>
       ),
     },
-  ];
+  ]
 
   // Dialog open & close
-  const [selectedCase, setSelectedCase] = useState<CaseItem | null>(null);
+  const [selectedCase, setSelectedCase] = useState<CaseItem | null>(null)
 
-  const handleCloseDialog = () => setSelectedCase(null);
+  const handleCloseDialog = () => setSelectedCase(null)
 
   const { dataGridProps } = useDataGrid<CaseItem>({
-      resource: "cases",
-      initialCurrent: 1,
-      initialPageSize: 10,
-      initialSorter: [
-        {
-          field: "ID",
-          order: "asc",
-        },
-      ],
-    })
+    resource: "cases",
+    initialCurrent: 1,
+    initialPageSize: 10,
+    initialSorter: [
+      {
+        field: "ID",
+        order: "asc",
+      },
+    ],
+  })
 
   return (
     <div>
@@ -204,10 +169,10 @@ export default function CoachDropdown({ item }: { item: CaseItem }) {
       />
       <DataGrid {...dataGridProps} columns={columns} autoHeight pageSizeOptions={[10, 20, 30, 50, 100]} />
       {selectedCase && (
-        <DetailedCaseView onClose={handleCloseDialog} caseDetails={selectedCase} caseID={selectedCase.id}/>
+        <DetailedCaseView onClose={handleCloseDialog} caseDetails={selectedCase} caseID={selectedCase.id} />
       )}
     </div>
-  );
+  )
 }
 
 const coachList: Coach[] = [
--- frontend/src/pages/detailed-case-view.tsx
+++ frontend/src/pages/detailed-case-view.tsx
@@ -1,208 +1,217 @@
-import dayjs from 'dayjs';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
-import { Dialog, DialogTitle, DialogContent, Typography, Box, Link, TextField } from "@mui/material";
+import { Box, Dialog, DialogContent, DialogTitle, Link, TextField, Typography } from "@mui/material"
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker"
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider"
 import { useUpdate } from "@refinedev/core"
+import dayjs from "dayjs"
+import React, { useRef } from "react"
 import { type CaseItem } from "../types"
-import React, { useRef } from "react";
 
 interface DetailedCaseViewProps {
-  caseID: number;
-  caseDetails: CaseItem | null;
-  onClose: () => void;
+  caseID: number
+  caseDetails: CaseItem | null
+  onClose: () => void
 }
 
 export default function DetailedCaseView({ onClose, caseID, caseDetails }: DetailedCaseViewProps) {
   const { mutate } = useUpdate()
-  const cancelRef = useRef<(() => void) | null>(null);
-  const [notes, setNotes] = React.useState(caseDetails?.notes ?? '');
+  const cancelRef = useRef<(() => void) | null>(null)
+  const [notes, setNotes] = React.useState(caseDetails?.notes ?? "")
 
   const updateCase = () => {
-    mutate({
-      resource: "cases",
-      values: {
-        ...caseDetails,
-        notes: notes,
-      },
-      id: caseID,
-      meta: {
-        method: "put",
-      }
-    }, {
-        onSuccess: () => {
-          setIsEditing(false);
-          onClose();
+    mutate(
+      {
+        resource: "cases",
+        values: {
+          ...caseDetails,
+          notes: notes,
         },
-      });
+        id: caseID,
+        meta: {
+          method: "put",
+        },
+      },
+      {
+        onSuccess: () => {
+          setIsEditing(false)
+          onClose()
+        },
+      },
+    )
   }
 
   const cancelUpdate = () => {
-    cancelRef.current?.();
-    setIsEditing(false);
-    setNotes(caseDetails?.notes ?? '');
-  };
+    cancelRef.current?.()
+    setIsEditing(false)
+    setNotes(caseDetails?.notes ?? "")
+  }
 
-  const [isEditing, setIsEditing] = React.useState(false);
+  const [isEditing, setIsEditing] = React.useState(false)
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setIsEditing(true);
-  };
+    e.preventDefault()
+    setIsEditing(true)
+  }
 
   const startEditing = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setIsEditing(true);
-  };
+    e.preventDefault()
+    setIsEditing(true)
+  }
 
   const handleNotesChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNotes(event.target.value);
-  };
+    setNotes(event.target.value)
+  }
 
   return (
     <Dialog open={!!caseDetails} onClose={onClose}>
-        <>
-          <DialogTitle>Case: {caseDetails?.client.name}</DialogTitle>
-            <DialogContent>
-              <form onSubmit={isEditing ? onSubmit : startEditing}>
-                <Box component="form" noValidate autoComplete="off">
-                    <TextField
-                        margin="dense"
-                        id="id"
-                        label="ID"
-                        type="text"
-                        fullWidth
-                        variant="outlined"
-                        value={caseDetails?.id ?? ''}
-                        InputProps={{
-                          readOnly: true
-                        }}
-                    />
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DateTimePicker
-                            label="Start Time"
-                            value={caseDetails ? dayjs(caseDetails.startTime) : null}
-                            readOnly
-                        />
-                    </LocalizationProvider>
-                    <TextField
-                        margin="dense"
-                        id="clientName"
-                        label="Client Name"
-                        type="text"
-                        fullWidth
-                        variant="outlined"
-                        value={caseDetails?.client.name ?? ''}
-                        InputProps={{
-                          readOnly: true
-                        }}
-                    />
-                    <TextField
-                        margin="dense"
-                        id="clientEmail"
-                        label="Client Email"
-                        type="email"
-                        fullWidth
-                        variant="outlined"
-                        value={caseDetails?.client.email ?? ''}
-                        InputProps={{
-                          readOnly: true
-                        }}
-                    />
-                    <TextField
-                        margin="dense"
-                        id="clientPhone"
-                        label="Client Phone"
-                        type="tel"
-                        fullWidth
-                        variant="outlined"
-                        value={caseDetails?.client.phone ?? ''}
-                        InputProps={{
-                          readOnly: true
-                        }}
-                    />
-                    <TextField
-                        margin="dense"
-                        id="clientZip"
-                        label="ZIP Code"
-                        type="text"
-                        fullWidth
-                        variant="outlined"
-                        value={caseDetails?.client.zip ?? ''}
-                        InputProps={{
-                          readOnly: true
-                        }}
-                    />
-                    <Typography margin="dense" variant="body1" component="div">
-                            Profile URL:
-                        </Typography>
-                        <Link
-                            href={caseDetails?.client.profile ? (caseDetails.client.profile.startsWith('https://') ? caseDetails.client.profile : `https://${caseDetails.client.profile}`) : '#'}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            display="block"
-                            variant="body1"
-                            style={{ marginBottom: '16px' }}
-                        >
-                            {caseDetails?.client.profile ?? 'Not available'}
-                        </Link>
-                    <TextField
-                        margin="dense"
-                        id="coachesNames"
-                        label="Coaches"
-                        type="text"
-                        fullWidth
-                        variant="outlined"
-                        value={caseDetails?.coaches?.map(coach => coach.name).join(', ') ?? 'None'}
-                        InputProps={{
-                          readOnly: true
-                        }}
-                    />
-                    <TextField
-                        margin="dense"
-                        id="language"
-                        label="Language"
-                        type="text"
-                        fullWidth
-                        variant="outlined"
-                        value={caseDetails?.data.language ?? ''}
-                        InputProps={{
-                          readOnly: true
-                        }}
-                    />
-                    <TextField
-                        margin="dense"
-                        id="benefits"
-                        label="Benefits"
-                        type="text"
-                        fullWidth
-                        variant="outlined"
-                        value={caseDetails?.data.benefits ?? ''}
-                        InputProps={{
-                          readOnly: true
-                        }}
-                    />
-                    <TextField
-                        margin="dense"
-                        id="notes"
-                        label="Notes"
-                        type="text"
-                        fullWidth
-                        variant="outlined"
-                        defaultValue={caseDetails?.notes ?? ''}
-                        onChange={handleNotesChange}
-                        InputProps={{
-                          readOnly: !isEditing
-                        }}
-                    />
-                </Box>
-                  {isEditing? (<>
+      <>
+        <DialogTitle>Case: {caseDetails?.client.name}</DialogTitle>
+        <DialogContent>
+          <form onSubmit={isEditing ? onSubmit : startEditing}>
+            <Box component="form" noValidate autoComplete="off">
+              <TextField
+                margin="dense"
+                id="id"
+                label="ID"
+                type="text"
+                fullWidth
+                variant="outlined"
+                value={caseDetails?.id ?? ""}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DateTimePicker label="Start Time" value={caseDetails ? dayjs(caseDetails.startTime) : null} readOnly />
+              </LocalizationProvider>
+              <TextField
+                margin="dense"
+                id="clientName"
+                label="Client Name"
+                type="text"
+                fullWidth
+                variant="outlined"
+                value={caseDetails?.client.name ?? ""}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+              <TextField
+                margin="dense"
+                id="clientEmail"
+                label="Client Email"
+                type="email"
+                fullWidth
+                variant="outlined"
+                value={caseDetails?.client.email ?? ""}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+              <TextField
+                margin="dense"
+                id="clientPhone"
+                label="Client Phone"
+                type="tel"
+                fullWidth
+                variant="outlined"
+                value={caseDetails?.client.phone ?? ""}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+              <TextField
+                margin="dense"
+                id="clientZip"
+                label="ZIP Code"
+                type="text"
+                fullWidth
+                variant="outlined"
+                value={caseDetails?.client.zip ?? ""}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+              <Typography margin="dense" variant="body1" component="div">
+                Profile URL:
+              </Typography>
+              <Link
+                href={
+                  caseDetails?.client.profile
+                    ? caseDetails.client.profile.startsWith("https://")
+                      ? caseDetails.client.profile
+                      : `https://${caseDetails.client.profile}`
+                    : "#"
+                }
+                target="_blank"
+                rel="noopener noreferrer"
+                display="block"
+                variant="body1"
+                style={{ marginBottom: "16px" }}
+              >
+                {caseDetails?.client.profile ?? "Not available"}
+              </Link>
+              <TextField
+                margin="dense"
+                id="coachesNames"
+                label="Coaches"
+                type="text"
+                fullWidth
+                variant="outlined"
+                value={caseDetails?.coaches?.map((coach) => coach.name).join(", ") ?? "None"}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+              <TextField
+                margin="dense"
+                id="language"
+                label="Language"
+                type="text"
+                fullWidth
+                variant="outlined"
+                value={caseDetails?.data.language ?? ""}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+              <TextField
+                margin="dense"
+                id="benefits"
+                label="Benefits"
+                type="text"
+                fullWidth
+                variant="outlined"
+                value={caseDetails?.data.benefits ?? ""}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+              <TextField
+                margin="dense"
+                id="notes"
+                label="Notes"
+                type="text"
+                fullWidth
+                variant="outlined"
+                defaultValue={caseDetails?.notes ?? ""}
+                onChange={handleNotesChange}
+                InputProps={{
+                  readOnly: !isEditing,
+                }}
+              />
+            </Box>
+            {isEditing ? (
+              <>
                 <button onClick={updateCase}>Confirm</button>
                 <button onClick={cancelUpdate}>Cancel</button>
-              </>) : (<button type="submit">Edit case</button>)}
-              </form>
-            </DialogContent>
-        </>
+              </>
+            ) : (
+              <button type="submit">Edit case</button>
+            )}
+          </form>
+        </DialogContent>
+      </>
     </Dialog>
-  );
+  )
 }
