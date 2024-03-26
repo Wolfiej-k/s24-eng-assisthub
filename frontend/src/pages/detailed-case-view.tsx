86c0d19ef940009b394d7af4d5f91033f997b476
Hong Nhung Le <133190574+michelleln@users.noreply.github.com>
2024-03-25 20:20:21 -0400

    Update detailed-case-view.tsx

--- frontend/src/pages/detailed-case-view.tsx
+++ frontend/src/pages/detailed-case-view.tsx
@@ -1,192 +1,70 @@
-// import React from "react";
-// import { useForm, HttpError, BaseKey, useSelect } from "@refinedev/core";
-// import { Dialog, DialogTitle, DialogContent, TextField } from "@mui/material";
-// //import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
-// //import DatePicker from "react-datepicker";
-// //import "react-datepicker/dist/react-datepicker.css";
-// import dayjs from 'dayjs';
-// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-// import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-// import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
-// import Box from '@mui/material/Box';
-
-// interface DetailedCaseViewProps {
-//   eventID: number | null;
-//   onClose: () => void;
-// }
-
-// interface IImage {
-//   url: string;
-//   name?: string; // Optional, add more fields as needed
-//   status?: string;
-//   type?: string;
-//   uid?: string;
-// }
-
-// interface IPost {
-//   id: BaseKey;
-//   title: string;
-//   content: string;
-//   hit: number;
-//   status: "draft" | "rejected" | "published";
-//   createdAt: Date;
-//   publishedAt: Date;
-//   language: number;
-//   image: IImage[]
-// }
-
-// interface FormValues {
-//   title?: string;
-//   content?: string;
-//   hit?: number;
-//   status?: "draft" | "rejected" | "published";
-//   createdAt?: Date;
-//   publishedAt?: Date;
-//   language?: number;
-//   image?: IImage[]
-// }
-
-// const DetailedCaseView: React.FC<DetailedCaseViewProps> = ({ eventID, onClose }) => {
-//   const { queryResult, formLoading, onFinish } = useForm<IPost, HttpError, FormValues>({
-//     resource: "posts",
-//     action: "edit",
-//     id: eventID?.toString(),
-//     redirect: "show", // redirect to show page after form submission, defaults to "list"
-//   });
-
-//   const currentValues = queryResult?.data?.data;
-
-//   const [values, setValues] = React.useState<FormValues>({
-//     title: currentValues?.title,
-//     content: currentValues?.content,
-//     hit: currentValues?.hit,
-//     status: currentValues?.status,
-//     createdAt: currentValues?.createdAt,
-//     publishedAt: currentValues?.publishedAt,
-//     language: currentValues?.language,
-//     image: currentValues?.image
-//   });
-
-//   const [initialValues, setInitialValues] = React.useState<FormValues>({
-//     title: currentValues?.title,
-//     content: currentValues?.content,
-//     hit: currentValues?.hit,
-//     status: currentValues?.status,
-//     createdAt: currentValues?.createdAt,
-//     publishedAt: currentValues?.publishedAt,
-//     language: currentValues?.language,
-//     image: currentValues?.image
-//   });
-
-//   React.useEffect(() => {
-//     setValues({
-//       title: currentValues?.title,
-//       content: currentValues?.content,
-//       hit: currentValues?.hit,
-//       status: currentValues?.status,
-//       createdAt: currentValues?.createdAt,
-//       publishedAt: currentValues?.publishedAt,
-//       language: currentValues?.language,
-//       image: currentValues?.image
-//     });
-// }, [eventID, currentValues]);
-
-//   const onSubmit = (e) => {
-//     e.preventDefault();
-//     onFinish(values);
-//     setIsEditing(false);
-//   };
-
-//   const [isEditing, setIsEditing] = React.useState(false);
-
-//   const startEditing = (e) => {
-//     e.preventDefault();
-//     setIsEditing(true);
-//   };
-
-//   const cancelEditing = (e) => {
-//     e.preventDefault();
-//     setValues(initialValues);
-//     setIsEditing(false);
-//   };
-
-//   return (
-//     <Dialog open={!!eventID} onClose={onClose}>
-//         <>
-//           <DialogTitle>Case: {values.title}</DialogTitle>
-//           <DialogContent>
-//             <label>ID: </label>
-//             <TextField value={eventID} fullWidth disabled/>
-//             <form onSubmit={isEditing ? onSubmit : startEditing} id="external-form">
-//             <label>Content: </label>
-//             <TextField fullWidth multiline rows={6} disabled={!isEditing}
-//               value={values.content}
-//               onChange={(e) => setValues({ ...values, content: e.target.value })}
-//             />
-//             <label>Created At: </label>
-//             <LocalizationProvider dateAdapter={AdapterDayjs}>
-//               <Box width={300}>
-//                 <DateTimePicker
-//                   disabled={!isEditing}
-//                   value={dayjs(values.createdAt)}
-//                   onChange={(date) => setValues({ ...values, createdAt: date?.toDate() })}
-//                 />
-//               </Box>
-//             </LocalizationProvider>
-//             <label>Status: </label>
-//             <select value = {values.status} onChange={(e) => {
-//               const newValue: "draft" | "rejected" | "published" = e.target.value as "draft" | "rejected" | "published"
-//               setValues({ ...values, status: newValue })}} disabled={!isEditing}>
-//               <option value="published">published</option>
-//               <option value="draft">draft</option>
-//               <option value="rejected">rejected</option>
-//             </select>
-//             {values.image && (
-//               <>
-//                 <br />
-//                 <label>Image: </label>
-//                 <br />
-
-//                 <img
-//                   src={values.image[0].url}
-//                   width={200}
-//                   height={200}
-//                 />
-//                 <br />
-//                 <br />
-//               </>
-//             )}
-//             {isEditing? (<>
-//               <button type="submit">Confirm</button>
-//               <button type="button" onClick={cancelEditing}>Cancel</button>
-//             </>) : (<button type="submit">Edit case</button>)}
-//             </form>
-//           </DialogContent>
-//         </>
-//     </Dialog>
-//   );
-// };
-
-// export default DetailedCaseView;
-
 import dayjs from 'dayjs';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import { Dialog, DialogTitle, DialogContent, Typography, Box, Link, TextField } from "@mui/material";
+import { useUpdate } from "@refinedev/core"
 import { type CaseItem } from "../types"
+import React, { useRef } from "react";
 
 interface DetailedCaseViewProps {
+  caseID: number;
   caseDetails: CaseItem | null;
   onClose: () => void;
 }
 
-export default function DetailedCaseView({ onClose, caseDetails }: DetailedCaseViewProps) {
+export default function DetailedCaseView({ onClose, caseID, caseDetails }: DetailedCaseViewProps) {
+  const { mutate } = useUpdate()
+  const cancelRef = useRef<(() => void) | null>(null);
+  const [notes, setNotes] = React.useState(caseDetails?.notes ?? '');
+
+  const updateCase = () => {
+    mutate({
+      resource: "cases",
+      values: {
+        ...caseDetails,
+        notes: notes,
+      },
+      id: caseID,
+      meta: {
+        method: "put",
+      }
+    }, {
+        onSuccess: () => {
+          setIsEditing(false);
+          onClose();
+        },
+      });
+  }
+
+  const cancelUpdate = () => {
+    cancelRef.current?.();
+    setIsEditing(false);
+    setNotes(caseDetails?.notes ?? '');
+  };
+
+  const [isEditing, setIsEditing] = React.useState(false);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsEditing(true);
+  };
+
+  const startEditing = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsEditing(true);
+  };
+
+  const handleNotesChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNotes(event.target.value);
+  };
+
   return (
     <Dialog open={!!caseDetails} onClose={onClose}>
         <>
-            <DialogTitle>Case: {caseDetails?.client.name}</DialogTitle>
+          <DialogTitle>Case: {caseDetails?.client.name}</DialogTitle>
             <DialogContent>
+              <form onSubmit={isEditing ? onSubmit : startEditing}>
                 <Box component="form" noValidate autoComplete="off">
                     <TextField
                         margin="dense"
@@ -196,8 +74,17 @@ export default function DetailedCaseView({ onClose, caseDetails }: DetailedCaseV
                         fullWidth
                         variant="outlined"
                         value={caseDetails?.id ?? ''}
-                        InputProps={{ readOnly: true }}
+                        InputProps={{
+                          readOnly: true
+                        }}
                     />
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DateTimePicker
+                            label="Start Time"
+                            value={caseDetails ? dayjs(caseDetails.startTime) : null}
+                            readOnly
+                        />
+                    </LocalizationProvider>
                     <TextField
                         margin="dense"
                         id="clientName"
@@ -206,7 +93,9 @@ export default function DetailedCaseView({ onClose, caseDetails }: DetailedCaseV
                         fullWidth
                         variant="outlined"
                         value={caseDetails?.client.name ?? ''}
-                        InputProps={{ readOnly: true }}
+                        InputProps={{
+                          readOnly: true
+                        }}
                     />
                     <TextField
                         margin="dense"
@@ -216,7 +105,9 @@ export default function DetailedCaseView({ onClose, caseDetails }: DetailedCaseV
                         fullWidth
                         variant="outlined"
                         value={caseDetails?.client.email ?? ''}
-                        InputProps={{ readOnly: true }}
+                        InputProps={{
+                          readOnly: true
+                        }}
                     />
                     <TextField
                         margin="dense"
@@ -226,7 +117,9 @@ export default function DetailedCaseView({ onClose, caseDetails }: DetailedCaseV
                         fullWidth
                         variant="outlined"
                         value={caseDetails?.client.phone ?? ''}
-                        InputProps={{ readOnly: true }}
+                        InputProps={{
+                          readOnly: true
+                        }}
                     />
                     <TextField
                         margin="dense"
@@ -236,7 +129,9 @@ export default function DetailedCaseView({ onClose, caseDetails }: DetailedCaseV
                         fullWidth
                         variant="outlined"
                         value={caseDetails?.client.zip ?? ''}
-                        InputProps={{ readOnly: true }}
+                        InputProps={{
+                          readOnly: true
+                        }}
                     />
                     <Typography margin="dense" variant="body1" component="div">
                             Profile URL:
@@ -247,7 +142,7 @@ export default function DetailedCaseView({ onClose, caseDetails }: DetailedCaseV
                             rel="noopener noreferrer"
                             display="block"
                             variant="body1"
-                            style={{ marginBottom: '16px' }} // Adjust the margin as needed
+                            style={{ marginBottom: '16px' }}
                         >
                             {caseDetails?.client.profile ?? 'Not available'}
                         </Link>
@@ -259,7 +154,9 @@ export default function DetailedCaseView({ onClose, caseDetails }: DetailedCaseV
                         fullWidth
                         variant="outlined"
                         value={caseDetails?.coaches?.map(coach => coach.name).join(', ') ?? 'None'}
-                        InputProps={{ readOnly: true }}
+                        InputProps={{
+                          readOnly: true
+                        }}
                     />
                     <TextField
                         margin="dense"
@@ -269,7 +166,9 @@ export default function DetailedCaseView({ onClose, caseDetails }: DetailedCaseV
                         fullWidth
                         variant="outlined"
                         value={caseDetails?.data.language ?? ''}
-                        InputProps={{ readOnly: true }}
+                        InputProps={{
+                          readOnly: true
+                        }}
                     />
                     <TextField
                         margin="dense"
@@ -279,16 +178,29 @@ export default function DetailedCaseView({ onClose, caseDetails }: DetailedCaseV
                         fullWidth
                         variant="outlined"
                         value={caseDetails?.data.benefits ?? ''}
-                        InputProps={{ readOnly: true }}
+                        InputProps={{
+                          readOnly: true
+                        }}
                     />
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DateTimePicker
-                            label="Start Time"
-                            value={caseDetails ? dayjs(caseDetails.startTime) : null}
-                            readOnly
-                        />
-                    </LocalizationProvider>
+                    <TextField
+                        margin="dense"
+                        id="notes"
+                        label="Notes"
+                        type="text"
+                        fullWidth
+                        variant="outlined"
+                        defaultValue={caseDetails?.notes ?? ''}
+                        onChange={handleNotesChange}
+                        InputProps={{
+                          readOnly: !isEditing
+                        }}
+                    />
                 </Box>
+                  {isEditing? (<>
+                <button onClick={updateCase}>Confirm</button>
+                <button onClick={cancelUpdate}>Cancel</button>
+              </>) : (<button type="submit">Edit case</button>)}
+              </form>
             </DialogContent>
         </>
     </Dialog>
