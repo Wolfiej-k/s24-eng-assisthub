8b53bca04c28ccc77d2fae7659763393831d99ba
Julia Poulson <jjpoulson@college.harvard.edu>
2024-04-04 14:46:21 -0400

    Changed GET method, added documentation

--- backend/src/api/cases.ts
+++ backend/src/api/cases.ts
@@ -3,14 +3,33 @@ import { CaseModel, type Case } from "../schemas/case.js"
 
 const router = Router()
 
-router.get("/", async (_req, res) => {
-  const items = await CaseModel.find()
-  res.status(200).json(items)
+router.get("/", async (req, res) => { //Get a list of cases
+  const items = await CaseModel.find({})
+  res.status(200).json(items) // Check for error
+  // Take in a list of specifications
+  const _sort = req.query._sort;
+  const _order = req.query._order;
+  const _start = req.query._start;
+  const _end = req.query._end;
+  let sorted_order = 1
+  type SortOrder = 1 | -1;
+
+  if (_sort){
+    if (_order){
+      if (_order == 'desc') {sorted_order = -1} //Override automatic ascending order if order is specified as descending
+    }
+    const result = await CaseModel.find({}).sort({_sort: sorted_order as SortOrder}) //Sort CaseModel
+
+    if(_start && _end){
+      return result[_start as unknown as number, _end as unknown as number] //If start and end points are specified, return only between those indices
+    }
+    return result //Otherwise, return the entire array
+  }
 })
 
-router.post("/", async (req, res) => {
+router.post("/", async (req, res) => { //Post a case
   const { client, coaches, data, startTime, endTime, notes } = req.body as Case
-  const item = new CaseModel({
+  const item = new CaseModel({ //Create new case with input
     client: client,
     coaches: coaches,
     data: data,
@@ -23,28 +42,28 @@ router.post("/", async (req, res) => {
     await item.save()
     res.status(201).json(item)
   } catch {
-    res.status(400).json({ error: "Validation failed" })
+    res.status(400).json({ error: "Validation failed" }) //Validate for errors
   }
 })
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", async (req, res) => { //Get specific case
   try {
-    const item = await CaseModel.findById(req.params.id)
+    const item = await CaseModel.findById(req.params.id) //Choose case by id
     if (item) {
       res.status(200).json(item)
     } else {
       res.status(404).json({ error: "Not found" })
     }
   } catch {
-    res.status(404).json({ error: "Not found" })
+    res.status(404).json({ error: "Not found" }) //Validate for errors
   }
 })
 
-router.patch("/:id", async (req, res) => {
+router.patch("/:id", async (req, res) => { //Update case
   try {
     const item = await CaseModel.findById(req.params.id)
     if (item) {
-      const { client, coaches, data, startTime, endTime, notes } = req.body as Case
+      const { client, coaches, data, startTime, endTime, notes } = req.body as Case //Update case by id
       item.client = client ?? item.client
       item.coaches = coaches ?? item.coaches
       item.data = data ?? item.data
@@ -56,7 +75,7 @@ router.patch("/:id", async (req, res) => {
         await item.save()
         res.status(201).json(item)
       } catch {
-        res.status(400).json({ error: "Validation failed" })
+        res.status(400).json({ error: "Validation failed" }) //Validate for errors
       }
     } else {
       res.status(404).json({ error: "Not found" })
@@ -66,12 +85,12 @@ router.patch("/:id", async (req, res) => {
   }
 })
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", async (req, res) => { //Delete case
   try {
-    await CaseModel.deleteOne({ _id: req.params.id })
+    await CaseModel.deleteOne({ _id: req.params.id }) //Delete case by id
     res.status(204).json()
   } catch {
-    res.status(404).json({ error: "Not found" })
+    res.status(404).json({ error: "Not found" }) //Validate for errors
   }
 })
 
--- readme.md
+++ readme.md
@@ -1,3 +1,24 @@
 ## AssistHub
 
 To be filled out.
+
+API DOCUMENTATION
+
+    GET:
+        There are two GET methods: one to retrieve a specific case, and one to retrieve an array of cases. 
+
+        For the specific case, the function simply takes in the case id and returns the case.
+
+        To retrieve several cases, the function takes in several parameters: _sort, _order, _start, _end.
+            _sort takes the value of the name of one of the case fields (ex. client.name), indicating which field is to be sorted by
+            _order is either 'asc' or 'desc' indicating the order the sorted list should be outputted in 
+            _start and _end are some indices, indicating which indices of the sorted array we display
+
+    POST:
+        The POST method allows users to create new cases. They do so by creating a new CaseModel object, taking in all the information associated with a case model (including client, an array of coaches, data, and notes), and autogenerating an id and start time.
+
+    DELETE:
+        The DELETE method takes in a case id, and deletes that case from the database.
+
+    PATCH:
+        The PATCH method takes an id and a case body, and alters the case with that id according to the case inputted.
