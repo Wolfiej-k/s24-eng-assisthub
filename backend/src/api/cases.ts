85ef191d72d239cbf16ccdf677129d47a8d090d3
Taj Jethwani-Keyser <tjethwanikeyser@college.harvard.edu>
2024-04-17 23:09:45 -0400

    readded logic for put, patch

--- backend/src/api/cases.ts
+++ backend/src/api/cases.ts
@@ -4,12 +4,12 @@ import { CaseModel, type Case } from "../schemas/case.js"
 const router = Router()
 
 router.get("/", async (req, res) => {
-  let items
+  const filter: Record<string, string> = {}
   if (!req.auth.admin) {
-    items = await CaseModel.find({ coaches: req.auth.identity }).populate("coaches")
-  } else {
-    items = await CaseModel.find().populate("coaches")
+    filter.coaches = req.auth.identity._id
   }
+
+  const items = await CaseModel.find(filter).populate("coaches")
   res.status(200).json(items)
 })
 
@@ -35,12 +35,13 @@ router.post("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-    const item = await CaseModel.findById(req.params.id).populate("coaches")
+    let item = await CaseModel.findById(req.params.id)
     if (item) {
       if (req.auth.admin || req.auth.identity._id in item.coaches) {
+        item = await item.populate("coaches")
         res.status(200).json(item)
       } else {
-        res.status(401).json("Not authorized")
+        res.status(401).json({ error: "Not authorized" })
       }
     } else {
       res.status(404).json({ error: "Not found" })
@@ -54,20 +55,24 @@ router.patch("/:id", async (req, res) => {
   try {
     let item = await CaseModel.findById(req.params.id)
     if (item) {
-      const { client, coaches, data, startTime, endTime, notes } = req.body as Partial<Case>
-      item.client = client ?? item.client
-      item.coaches = coaches ?? item.coaches
-      item.data = data ?? item.data
-      item.startTime = startTime ?? item.startTime
-      item.endTime = endTime ?? item.endTime
-      item.notes = notes ?? item.notes
+      if (req.auth.admin || req.auth.identity._id in item.coaches) {
+        const { client, coaches, data, startTime, endTime, notes } = req.body as Partial<Case>
+        item.client = client ?? item.client
+        item.coaches = coaches ?? item.coaches
+        item.data = data ?? item.data
+        item.startTime = startTime ?? item.startTime
+        item.endTime = endTime ?? item.endTime
+        item.notes = notes ?? item.notes
 
-      try {
-        await item.save()
-        item = await item.populate("coaches")
-        res.status(201).json(item)
-      } catch {
-        res.status(400).json({ error: "Validation failed" })
+        try {
+          await item.save()
+          item = await item.populate("coaches")
+          res.status(201).json(item)
+        } catch {
+          res.status(400).json({ error: "Validation failed" })
+        }
+      } else {
+        res.status(401).json({ error: "Not authorized" })
       }
     } else {
       res.status(404).json({ error: "Not found" })
@@ -81,20 +86,24 @@ router.put("/:id", async (req, res) => {
   try {
     let item = await CaseModel.findById(req.params.id)
     if (item) {
-      const { client, coaches, data, startTime, endTime, notes } = req.body as Case
-      item.client = client
-      item.coaches = coaches
-      item.data = data
-      item.startTime = startTime
-      item.endTime = endTime
-      item.notes = notes
+      if (req.auth.admin || req.auth.identity._id in item.coaches) {
+        const { client, coaches, data, startTime, endTime, notes } = req.body as Case
+        item.client = client
+        item.coaches = coaches
+        item.data = data
+        item.startTime = startTime
+        item.endTime = endTime
+        item.notes = notes
 
-      try {
-        await item.save()
-        item = await item.populate("coaches")
-        res.status(201).json(item)
-      } catch {
-        res.status(400).json({ error: "Validation failed" })
+        try {
+          await item.save()
+          item = await item.populate("coaches")
+          res.status(201).json(item)
+        } catch {
+          res.status(400).json({ error: "Validation failed" })
+        }
+      } else {
+        res.status(401).json({ error: "Not authorized" })
       }
     } else {
       res.status(404).json({ error: "Not found" })
@@ -106,8 +115,6 @@ router.put("/:id", async (req, res) => {
 
 router.delete("/:id", async (req, res) => {
   try {
-    const item = await CaseModel.findById(req.params.id)
-
     await CaseModel.deleteOne({ _id: req.params.id })
     res.status(204).json()
   } catch {
