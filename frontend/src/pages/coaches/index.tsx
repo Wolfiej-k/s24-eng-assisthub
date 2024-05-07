9f1a519eb03074d13e61e27354ccbe5c818e2151
hannawosenu <hannaawos@gmail.com>
2024-05-07 16:00:06 -0400

    coaches page

--- frontend/src/pages/coaches/index.tsx
+++ frontend/src/pages/coaches/index.tsx
@@ -1,5 +1,12 @@
 import CreateCoachForm from "./create-coach"
+import CoachGrid from "./coach-grid"
 
 export default function CoachesPage() {
-  return <CreateCoachForm />
+  return (
+    <div>
+      <CreateCoachForm />
+      <div style={{ marginTop: '50px' }}></div>
+      <CoachGrid />
+    </div>
+  );
 }
