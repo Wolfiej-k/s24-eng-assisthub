9e2ff33a263e5d2b9fd224b167c0b7dd9b5d9ce8
hannawosenu <hannawosenu@users.noreply.github.com>
2024-05-07 20:01:26 +0000

    Apply formatting changes

--- frontend/src/pages/coaches/coach-grid.tsx
+++ frontend/src/pages/coaches/coach-grid.tsx
@@ -1,16 +1,14 @@
-import { DataGrid, type GridColDef, type GridRenderCellParams } from "@mui/x-data-grid"
+import { DataGrid, type GridColDef } from "@mui/x-data-grid"
+import { DeleteButton } from "@refinedev/antd"
 import { useDataGrid } from "@refinedev/mui"
 import { useMemo, useState } from "react"
 import { type Coach } from "../../types"
-import { DeleteButton } from "@refinedev/antd";
 
 export default function CoachGrid() {
   const [selectedCoach, setSelectedCoach] = useState<Coach | null>(null)
   const handleCloseDialog = () => setSelectedCoach(null)
 
-  const DeleteCoachButton = ({ _id }: { _id: string }) => (
-    <DeleteButton resource="coaches" recordItemId={_id} />
-  );
+  const DeleteCoachButton = ({ _id }: { _id: string }) => <DeleteButton resource="coaches" recordItemId={_id} />
 
   const columns = useMemo<GridColDef<Coach>[]>(
     () => [
@@ -42,7 +40,7 @@ export default function CoachGrid() {
         minWidth: 120,
         maxWidth: 300,
         flex: 1,
-        valueGetter: (params) => params.row.admin ? "YES" : "NO",
+        valueGetter: (params) => (params.row.admin ? "YES" : "NO"),
         renderCell: (params) => (
           <div style={{ overflow: "hidden", whiteSpace: "normal", wordWrap: "break-word" }}>{params.value}</div>
         ),
@@ -55,10 +53,8 @@ export default function CoachGrid() {
         sortable: false,
         hideable: false,
         filterable: false,
-        renderCell: (params) => (
-          <DeleteCoachButton _id={params.row._id} />
-        ),
-      }
+        renderCell: (params) => <DeleteCoachButton _id={params.row._id} />,
+      },
     ],
     [],
   )
--- frontend/src/pages/coaches/index.tsx
+++ frontend/src/pages/coaches/index.tsx
@@ -1,12 +1,12 @@
-import CreateCoachForm from "./create-coach"
 import CoachGrid from "./coach-grid"
+import CreateCoachForm from "./create-coach"
 
 export default function CoachesPage() {
   return (
     <div>
       <CreateCoachForm />
-      <div style={{ marginTop: '50px' }}></div>
+      <div style={{ marginTop: "50px" }}></div>
       <CoachGrid />
     </div>
-  );
+  )
 }
